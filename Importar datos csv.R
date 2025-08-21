d5f789e0def2bdc5e4a604bc83ec88def35c46b5
Jorge Garza <ji_tigres@hotmail.com>
2025-08-21 11:49:39 -0600

    Importar datos locales y de la web

--- Importar datos csv.R
+++ Importar datos csv.R
@@ -14,14 +14,22 @@ summary(temperatura) # Resumen estadistico  sencillo de la base de datos
 names(temperatura) <- c("Anual", "Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", 
                         "Sep", "Oct", "Nov", "Dic")
 
-temperatura$media_anual <- rowMeans(temperatura[,2:13])
+temperatura$media_anual <- round(rowMeans(temperatura[,2:13]),1)
 head(temperatura)
 
 colores <- c("blue", "skyblue", "white")
 
 temp <- temperatura[ ,2:13]
+temp10 <- temperatura[11:21 , 2:13]
+
+# Crear un boxplot de las medias de temperatura del 2000 al 2020
 boxplot(temp, col = colores,
         main = "Medias de temperatura del 2000 al 2020",
+        xlab = "Meses", ylab = "Temperatura (°C)")
+
+# Crear un boxplot de las medias de temperatura del 2010 al 2020
+boxplot(temp10, col = colores,
+        main = "Medias de temperatura del 2010 al 2020",
         xlab = "Meses", ylab = "Temperatura (°C)")
 
 # Importar datos web ------------------------------------------------------
